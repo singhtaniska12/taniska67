b3f2db87d0d16e1bdd36203e5ba8b6f9db7f8884
singhtaniska12 <singhtaniska67@gmail.com>
2026-02-17 11:40:42 +0530

    Add OS module logging

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ const fs=require('fs');
 //});
 //fs.appendFileSync("./ABES.txt", 'Taniska', ()=>{});
 //fs.cp("./abes.txt","./a1.txt",()=>{})
-fs.appendFile("./a1.txt", " Welcome to A1 batch!", (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("File appended successfully!");
-    }
-});
+//fs.appendFile("./a1.txt", " Welcome to A1 batch!", (err) => {
+   // if (err) {
+   //     console.log(err);
+ //   } else {
+  //      console.log("File appended successfully!");
+  //  }
+//});
+const os=require('os');
+console.log("system platform",os.platform());
+console.log("user info",os.userInfo());
+console.log("cpu",os.arch());
+console.log("free memory",os.freemem());
+console.log("total memory",os.totalmem());
+console.log("uptime",os.uptime());
+console.log("home directory",os.homedir()); 
+console.log("hostname",os.hostname());
